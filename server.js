c89015fd5447bf7bd69ac2c7d37290d13f7a7c85
Glitch (glitch-hello-node) <none>
2021-05-07 00:58:51 +0000

    🏆🕴 Checkpoint ./server.js:31619339/925 ./package.json:31619339/58

--- server.js
+++ server.js
@@ -2,15 +2,25 @@ const Discord = require("discord.js");
 
 const client = new Discord.Client();
 
-const BOT_PREFIX = "-DND";
+const BOT_PREFIX = "-dnd";
 
 client.on("ready", () => {
   console.info("Bot is online!");
 });
 
+/**
+* Show Database
+* Modify in DB (Each user has a unique ID) (Username -ALPHANUMERIC, Character level -NUMBER, Progression -FLOAT)
+* Add a new user
+* Delete a user
+*/
+
 client.on("message", msg => {
-  if (msg.content === `$(BOT_PREFIX) DB`) {
-    msg.channel.send("show db");
+  
+  switch (msg.content.toLowerCase()) {
+    case `${BOT_PREFIX} db`:
+      break;
+    case `${BOT_PREFIX} Add `
   }
 });
 
